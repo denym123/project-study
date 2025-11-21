15f2ff9a0a414d94dfb93ce28edb62671a7c17dd
Dênym <denym.aq.filho@gmail.com>
2025-11-21 12:41:25 -0400

    corrigindo imports

--- lib/modules/products/products.dart
+++ lib/modules/products/products.dart
@@ -1,5 +1,8 @@
 export 'controllers/controllers.dart';
+export 'dtos/dtos.dart';
 export 'mixins/mixins.dart';
+export 'models/models.dart';
 export 'products_module.dart';
 export 'products_page.dart';
 export 'repositories/repositories.dart';
+export 'widgets/widgets.dart';
